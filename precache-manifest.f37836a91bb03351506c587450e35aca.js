37b0f2a4863d3b99716e176cc5dc49875af22bdf
ohethan <ohethan@live.unc.edu>
2019-10-13 02:51:01 -0400

    Updates

---
+++ precache-manifest.f37836a91bb03351506c587450e35aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee13fdd9bc44a200975ee9f6ab3be0da",
+    "url": "/minerva/index.html"
+  },
+  {
+    "revision": "1f3db20e8161276d826b",
+    "url": "/minerva/static/css/main.0cd381e7.chunk.css"
+  },
+  {
+    "revision": "0f55c23bfcaf37b0f38c",
+    "url": "/minerva/static/js/2.c15e480d.chunk.js"
+  },
+  {
+    "revision": "1f3db20e8161276d826b",
+    "url": "/minerva/static/js/main.7190fbfe.chunk.js"
+  },
+  {
+    "revision": "c3103377ca11d13018a0",
+    "url": "/minerva/static/js/runtime-main.1d768eff.js"
+  },
+  {
+    "revision": "3243946c278678a56a6a72d9f8768580",
+    "url": "/minerva/static/media/gradient.3243946c.png"
+  }
+]);
